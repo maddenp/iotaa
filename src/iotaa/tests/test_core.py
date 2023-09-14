bcfe1e9c8d6c26b00469fafcca7933bb0b408994
Paul Madden <maddenp@colorado.edu>
2023-09-14 02:14:31 +0000

    Unit tests @ 36%

--- src/iotaa/tests/test_core.py
+++ src/iotaa/tests/test_core.py
@@ -2,8 +2,7 @@
 Unit tests for module otaa.core.
 """
 
-# pylint: disable=missing-function-docstring
-# pylint: disable=redefined-outer-name # for pytest fixtures
+# pylint: disable=missing-function-docstring,protected-access,redefined-outer-name
 
 from unittest.mock import ANY, patch
 
@@ -32,3 +31,27 @@ def test_configure_logging_verbose():
     with patch.object(ic.logging, "basicConfig") as basicConfig:
         ic.configure_logging(verbose=True)
     basicConfig.assert_called_with(datefmt=ANY, format=ANY, level=ic.logging.DEBUG)
+
+
+def test_disable_dry_run():
+    with patch.object(ic, "_state", ic.ns(dry_run_enabled=True)):
+        assert ic._state.dry_run_enabled is True
+        ic.disable_dry_run()
+        assert ic._state.dry_run_enabled is False
+
+
+def test_enable_dry_run():
+    with patch.object(ic, "_state", ic.ns(dry_run_enabled=False)):
+        assert ic._state.dry_run_enabled is False
+        ic.enable_dry_run()
+        assert ic._state.dry_run_enabled is True
+
+
+def test_ids_dict():
+    ids = ic.ids(assets={"foo": ic.asset(id="bar", ready=lambda: True)})
+    assert ids["foo"] == "bar"
+
+
+def test_ids_list():
+    ids = ic.ids(assets=[ic.asset(id="bar", ready=lambda: True)])
+    assert ids[0] == "bar"
