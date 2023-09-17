f544fe6653b465f3f6c8b267361e9453bc109912
Paul Madden <maddenp@colorado.edu>
2023-09-17 20:19:22 +0000

    WIP

--- demo/teatime.py
+++ demo/teatime.py
@@ -8,7 +8,7 @@ from iotaa import asset, external, ids, task, tasks
 @tasks
 def a_cup_of_tea(basedir):
     yield f"A cup of steeped tea with sugar"
-    cupdir = ids(cup(basedir))[0]
+    cupdir = Path(ids(cup(basedir))[0])
     yield [cup(basedir), steeped_tea_with_sugar(cupdir)]
 
 
@@ -33,13 +33,15 @@ def steeped_tea_with_sugar(cupdir):
 def steeped_tea(cupdir):
     # Give tea time to steep.
     yield f"Steeped tea in {cupdir}"
+    ready = False
     water = ids(steeping_tea(cupdir))[0]
-    tea_time = dt.datetime.fromtimestamp(water.stat().st_mtime)
-    ready_time = tea_time + dt.timedelta(seconds=10)
-    now = dt.datetime.now()
-    ready = now >= ready_time
+    if water.exists():
+        tea_time = dt.datetime.fromtimestamp(water.stat().st_mtime)
+        ready_time = tea_time + dt.timedelta(seconds=10)
+        now = dt.datetime.now()
+        ready = now >= ready_time
     yield asset(None, lambda: ready)
-    if not ready:
+    if water.exists() and not ready:
         logging.info("Tea steeping for %ss more" % int((ready_time - now).total_seconds()))
     yield steeping_tea(cupdir)
 
@@ -54,14 +56,22 @@ def steeping_tea(cupdir):
 @task
 def tea_leaves(cupdir):
     # Place tea leaves in the cup.
-    for x in ingredient(cupdir, "leaves", "Tea leaves"):
+    for x in ingredient(cupdir, "leaves", "Tea leaves", tea_from_store):
         yield x
 
 
+@external
+def tea_from_store(cupdir):
+    path = cupdir.parent / "tea-package"
+    yield f"Tea from store: {path}"
+    yield asset(path, path.exists)
+
+
 def ingredient(cupdir, fn, name, req = None):
-    path = Path(cupdir) / fn
+    path = cupdir / fn
     yield f"{name} in {cupdir}"
     yield asset(path, path.exists)
     yield req(cupdir) if req else None
     path.parent.mkdir(parents=True, exist_ok=True)
+    logging.info(f"Adding {name.lower()} to {cupdir}")
     path.touch()
