ba71e27a1bb8606258c7a409c5d0c5d69c1e2602
Paul Madden <maddenp@colorado.edu>
2023-09-17 20:08:09 +0000

    WIP

--- demo/teatime.py
+++ demo/teatime.py
@@ -14,8 +14,9 @@ def a_cup_of_tea(basedir):
 
 @task
 def cup(basedir):
+    # Get a cup to make the tea in.
     path = Path(basedir) / "cup"
-    yield f"The Cup: {path}"
+    yield f"The cup: {path}"
     yield asset(path, path.exists)
     yield None
     path.mkdir(parents=True)
@@ -23,47 +24,44 @@ def cup(basedir):
 
 @task
 def steeped_tea_with_sugar(cupdir):
-    name = f"Steeped Tea with Sugar in {cupdir}"
-    for x in ingredient(cupdir, "sugar", name, steeped_tea):
+    # Add sugar to the steeped tea.
+    for x in ingredient(cupdir, "sugar", "Steeped tea with suagar", steeped_tea):
         yield x
 
 
-@external
+@task
 def steeped_tea(cupdir):
-    yield f"Steeped Tea in {cupdir}"
-    teapath = ids(tea(cupdir))[0]
-    tea_time = dt.datetime.fromtimestamp(teapath.stat().st_mtime)
+    # Give tea time to steep.
+    yield f"Steeped tea in {cupdir}"
+    water = ids(steeping_tea(cupdir))[0]
+    tea_time = dt.datetime.fromtimestamp(water.stat().st_mtime)
     ready_time = tea_time + dt.timedelta(seconds=10)
     now = dt.datetime.now()
     ready = now >= ready_time
-    if not ready:
-        logging.info("Tea still steeping: Wait %ss" % int((ready_time - now).total_seconds()))
     yield asset(None, lambda: ready)
+    if not ready:
+        logging.info("Tea steeping for %ss more" % int((ready_time - now).total_seconds()))
+    yield steeping_tea(cupdir)
 
 
-@tasks
-def tea(cupdir):
-    yield f"Boiling Water over Tea Leaves in {cupdir}"
-    yield [tea_leaves(cupdir), boiling_water(cupdir)]
+@task
+def steeping_tea(cupdir):
+    # Pour boiling water over the tea.
+    for x in ingredient(cupdir, "water", "Boiling water over tea leaves", tea_leaves):
+        yield x
 
 
 @task
 def tea_leaves(cupdir):
-    name = f"Tea Leaves in {cupdir}"
-    for x in ingredient(cupdir, "leaves", name):
-        yield x
-
-
-@task
-def boiling_water(cupdir):
-    name = f"Boiling Water in {cupdir}"
-    for x in ingredient(cupdir, "water", name):
+    # Place tea leaves in the cup.
+    for x in ingredient(cupdir, "leaves", "Tea leaves"):
         yield x
 
 
 def ingredient(cupdir, fn, name, req = None):
-    path = cupdir / fn
+    path = Path(cupdir) / fn
     yield f"{name} in {cupdir}"
     yield asset(path, path.exists)
     yield req(cupdir) if req else None
+    path.parent.mkdir(parents=True, exist_ok=True)
     path.touch()
