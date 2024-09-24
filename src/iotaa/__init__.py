b68c9ebd405a09cb9ec8737d33d4078a74074a46
Paul Madden <maddenp@colorado.edu>
2024-09-24 13:56:50 +0000

    task-tree graphics

--- src/iotaa/__init__.py
+++ src/iotaa/__init__.py
@@ -80,7 +80,8 @@ class Node:
         PM WRITEME.
         """
         node.dry_run = dry_run
-        _log.debug("  " * level + node.taskname)
+        junction = "├─ " if level else ""
+        _log.debug("│  " * (level-1) + junction + node.taskname)
         assert self.graph is not None
         self.graph.add(node)
         predecessor: Node
@@ -95,7 +96,7 @@ class Node:
         if self.root and not self.graph:
             self.graph = TopologicalSorter()
             _log.debug("Task tree")
-            _log.debug("---------")
+            _log.debug("─────────")
             self._assemble(self, dry_run)
             for node in self.graph.static_order():
                 node(dry_run)
