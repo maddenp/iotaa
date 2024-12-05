5f59e0aa59c5f870e77e6fc5dcda24483ba0f8b9
Paul Madden <maddenp@colorado.edu>
2024-12-05 06:21:02 +0000

    Work on pylint plugin

--- src/iotaa/pylint.py
+++ src/iotaa/pylint.py
@@ -19,18 +19,25 @@ def register(_: PyLinter) -> None:
 ARGNAME = "dry_run"
 
 
+def _accepts_argname(func: astroid.nodes.scoped_nodes.scoped_nodes.FunctionDef) -> bool:
+    """
+    Does argname appear in the function's arglist?
+    """
+    if args := getattr(func, "args", None):
+        return ARGNAME in [arg.name for arg in args.args]
+    return False
+
+
 def _looks_like_iotaa_task_call(node: astroid.Call) -> bool:
     """
     Does the node look like a call to an iotaa-decorated task function?
     """
-    # pylint: disable=too-many-boolean-expressions
     if (  # Ignore calls...
         Path(node.root().file).is_relative_to(sys.prefix)  # from stdlib / 3rd-party libs
         or not ARGNAME in [kw.arg for kw in node.keywords]  # that do not include argname
         or (func := safe_infer(node.func)) is astroid.Uninferable  # to uninferable functions
         or not (decorators := getattr(func, "decorators", None))  # to undecorated functions
-        or not (args := getattr(func, "args", None))  # to functions taking no arguments
-        or ARGNAME in [arg.name for arg in args.args]  # to functions that accept argname
+        or _accepts_argname(func)  # to functions that accept argname
     ):
         return False
     # Return the function if it is iotaa-decorated:
@@ -49,9 +56,11 @@ def _transform(node: astroid.Call) -> astroid.Call:
     # Remove the keyword argument:
     node.keywords = [kw for kw in node.keywords if kw.arg != ARGNAME]
     # Add a no-op statement to ensure the argument is still used:
-    stmt = astroid.parse(f"print({ARGNAME})").body[0]
-    stmt.parent = node.scope()
-    node.scope().body = [stmt, *node.scope().body]
+    hostfunc = node.scope()
+    if _accepts_argname(hostfunc):
+        stmt = astroid.parse(f"print({ARGNAME})").body[0]
+        stmt.parent = node.scope()
+        hostfunc.body = [stmt, *hostfunc.body]
 
 
 astroid.MANAGER.register_transform(astroid.Call, _transform, _looks_like_iotaa_task_call)
